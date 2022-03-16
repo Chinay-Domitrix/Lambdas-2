00446ee53e8f556dbef4d0be15001b069ad855ba
Chinay <55807470+Chinay-Domitrix@users.noreply.github.com>
2022-03-15 22:48:27 -0400

    Update PerfectSquares.kt
    Chirag commented on what he learned
    
    Signed-off-by: Chinay <55807470+Chinay-Domitrix@users.noreply.github.com>

--- src/main/kotlin/PerfectSquares.kt
+++ src/main/kotlin/PerfectSquares.kt
@@ -1,3 +1,10 @@
 fun interface PerfectSquares {
 	fun isPerfectSquare(n: Int): Boolean
 }
+/*
+What Chirag has learnt from this experience:
+	- Kotlin functional interfaces: very good 👍👍👍
+	- Lambda expressions: good 👍
+	- Git's mind-bending nature: bad 👎
+	- Trying to apply that mind-bending nature to follow a fixed branch diagram: very bad 👎👎👎
+*/
